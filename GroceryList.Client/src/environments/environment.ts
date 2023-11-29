225e14cbe8a39fb77af15d63f605a1c65aca6726
jeremy <jeremy@scotex.be>
2023-11-28 20:19:07 -0500

    wip traefik

--- GroceryList.Client/src/environments/environment.ts
+++ GroceryList.Client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    groceryListApiUrl: 'http://localhost:6578/api/grocerylists',
-    storeApiUrl: 'http://localhost:6578/api/stores',
-    userApiUrl: 'http://localhost:6578/api/users'
+    groceryListApiUrl: '',
+    storeApiUrl: '',
+    userApiUrl: ''
 };
