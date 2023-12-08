8d49aafac361821b5786e7b96320a06f5ad8719e
Jeremy Proot <jeremy.proot@outlook.com>
2023-12-08 11:01:00 -0500

    Added maps for mobile devices

--- GroceryList.Client/src/app/constants.ts
+++ GroceryList.Client/src/app/constants.ts
@@ -1,4 +1,6 @@
 export const GOOGLE_MAPS_QUERY: string = 'https://www.google.com/maps?q=';
+export const GOOGLE_MAPS_QUERY_ANDROID: string = 'https://maps.google.com/?q=';
+export const MAPS_QUERY_IPHONE: string = 'http://maps.apple.com/?q=';
 export const GEO_MOBILE_QUERY: string = 'geo:0,0?q=';
 
 export const ROUTES_PARAM = {
