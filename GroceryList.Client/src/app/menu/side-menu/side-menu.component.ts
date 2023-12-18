395c9bc1161a892d38ed3c7d577ddc3edf2c5cf8
Jeremy Proot <jeremy.proot@outlook.com>
2023-12-18 14:55:09 -0500

    added sidebar menu

---
+++ GroceryList.Client/src/app/menu/side-menu/side-menu.component.ts
@@ -0,0 +1,59 @@
+import { Component, Input, Renderer2, inject, HostListener, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { Select } from '@ngxs/store';
+import { AuthState } from '../../auth/ngxs-store/auth.state';
+import { Observable } from 'rxjs';
+import { NavigationStart, Router, RouterModule, RouterOutlet } from '@angular/router';
+import { SideMenuService } from './side-menu.service';
+
+@Component({
+  selector: 'app-side-menu',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.scss',
+  animations: [
+    trigger('sidebarAnimation', [
+      transition(':enter', [
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0
+        }),
+        animate('0.3s ease-in-out', style({
+          transform: 'translateX(0)',
+          opacity: 1
+        }))
+      ]),
+      transition(':leave', [
+        animate('0.3s ease-in-out', style({
+          transform: 'translateX(-100%)',
+          opacity: 0
+        }))
+      ])
+    ])
+  ]
+})
+export class SideMenuComponent implements OnInit {
+  #router = inject(Router);
+  #sideMenuService = inject(SideMenuService);
+  #renderer = inject(Renderer2);
+  isOpen = false;
+  @Select(AuthState.getName) name$!: Observable<string>;
+
+  ngOnInit(): void {
+    this.#router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.#sideMenuService.closeMenu();
+      }
+    });
+    this.#sideMenuService.isOpen$.subscribe(open => {
+      if (open) {
+        this.#renderer.addClass(document.body, 'sidebar-open');
+      } else {
+        this.#renderer.removeClass(document.body, 'sidebar-open');
+      }
+      this.isOpen = open;
+    });
+  }
+}
