299199b78ec39ea24c12d08042f165b8b28dcc2e
Jeremy Proot <jeremy.proot@outlook.com>
2024-01-18 11:51:50 -0500

    added effect instead of converting to observable

--- GroceryList.Client/src/app/shared/alert/alert.component.ts
+++ GroceryList.Client/src/app/shared/alert/alert.component.ts
@@ -1,9 +1,7 @@
-import { Component, inject, OnInit, OnDestroy, ChangeDetectorRef, computed, effect } from '@angular/core';
+import { Component, inject, ChangeDetectorRef, effect } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Subscription } from 'rxjs';
-import { toObservable } from '@angular/core/rxjs-interop'
 
-import { Alert, AlertType } from './alert.model';
+import { AlertType } from './alert.model';
 import { AlertService } from './alert.service';
 
 
@@ -14,17 +12,15 @@ import { AlertService } from './alert.service';
   templateUrl: './alert.component.html',
   styleUrl: './alert.component.scss'
 })
-export class AlertComponent implements OnInit {
+export class AlertComponent {
   #alertService = inject(AlertService);
   #cd= inject(ChangeDetectorRef)
   alert = this.#alertService.alert;
-  #alert$ = toObservable(this.#alertService.alert);
   showAlert: boolean = false;
 
-  ngOnInit(): void {
-    console.log(this.#alertService.alert().type !== AlertType.NoAlert);
-    this.#alert$.subscribe(alert => {
-      if (alert.type !== AlertType.NoAlert) {
+  constructor() {
+    effect(() => {
+      if (this.#alertService.alert().type !== AlertType.NoAlert) {
         this.showAlert = true;
         setTimeout(() => {this.showAlert = false; this.#cd.detectChanges();}, 5000)
       }
