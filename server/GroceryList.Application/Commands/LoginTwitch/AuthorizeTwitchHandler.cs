fcfe95c3382e13da728052cd4e3bdc3ff4df61cb
Jeremy Proot <jeremy.proot@outlook.com>
2024-09-25 21:21:13 +0200

    t

--- server/GroceryList.Application/Commands/LoginTwitch/AuthorizeTwitchHandler.cs
+++ server/GroceryList.Application/Commands/LoginTwitch/AuthorizeTwitchHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Web;
 
@@ -6,14 +7,17 @@ namespace GroceryList.Application.Commands.LoginTwitch;
 public class AuthorizeTwitchHandler : IRequestHandler<AuthorizeTwitchCommand, Result<string>>
 {
     private readonly TwitchOptions _twitchConfiguration;
+    private readonly ILogger<AuthorizeTwitchHandler> _logger;
 
-    public AuthorizeTwitchHandler(IOptions<TwitchOptions> options)
+    public AuthorizeTwitchHandler(IOptions<TwitchOptions> options, ILogger<AuthorizeTwitchHandler> logger)
     {
         _twitchConfiguration = options.Value;
+        _logger = logger;
     }
 
     public Task<Result<string>> Handle(AuthorizeTwitchCommand command, CancellationToken cancellationToken)
     {
+        _logger.LogInformation($"AuthorizeTwitch command: {_twitchConfiguration.AuthorizationEndpoint}");
         return Task.FromResult(Result<string>.Success($"{_twitchConfiguration.AuthorizationEndpoint}?response_type=code&client_id={_twitchConfiguration.ClientId}&redirect_uri={_twitchConfiguration.CallbackUrl}&scope={HttpUtility.UrlEncode($"{_twitchConfiguration.Scope}")}"));
     }
 }
