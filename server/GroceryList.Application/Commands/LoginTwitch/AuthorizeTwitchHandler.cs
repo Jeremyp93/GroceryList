1d4eb2c4adc96b8b4a83323b8eb838689d77a56d
Jeremy Proot <jeremy.proot@outlook.com>
2024-09-27 14:32:46 +0200

    t

--- server/GroceryList.Application/Commands/LoginTwitch/AuthorizeTwitchHandler.cs
+++ server/GroceryList.Application/Commands/LoginTwitch/AuthorizeTwitchHandler.cs
@@ -8,17 +8,14 @@ namespace GroceryList.Application.Commands.LoginTwitch;
 public class AuthorizeTwitchHandler : IRequestHandler<AuthorizeTwitchCommand, Result<string>>
 {
     private readonly TwitchOptions _twitchConfiguration;
-    private readonly ILogger<AuthorizeTwitchHandler> _logger;
 
-    public AuthorizeTwitchHandler(IOptions<TwitchOptions> options, ILogger<AuthorizeTwitchHandler> logger)
+    public AuthorizeTwitchHandler(IOptions<TwitchOptions> options)
     {
         _twitchConfiguration = options.Value;
-        _logger = logger;
     }
 
     public Task<Result<string>> Handle(AuthorizeTwitchCommand command, CancellationToken cancellationToken)
     {
-        _logger.LogInformation($"Login twitch options: {JsonSerializer.Serialize(_twitchConfiguration)}");
         return Task.FromResult(Result<string>.Success($"{_twitchConfiguration.AuthorizationEndpoint}?response_type=code&client_id={_twitchConfiguration.ClientId}&redirect_uri={_twitchConfiguration.CallbackUrl}&scope={HttpUtility.UrlEncode($"{_twitchConfiguration.Scope}")}"));
     }
 }
--- server/GroceryList.WebApi/Program.cs
+++ server/GroceryList.WebApi/Program.cs
@@ -5,16 +5,6 @@ using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-var configuration = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-    .AddJsonFile($"appsettings.{environment}.json", optional: true)
-    .AddEnvironmentVariables()
-    .Build();
-
-builder.Configuration.AddConfiguration(configuration);
-
 builder.Logging
   .ClearProviders()
   .AddConsole();
@@ -26,9 +16,6 @@ using var loggerFactory = LoggerFactory.Create(loggingBuilder => loggingBuilder
     .AddConsole());
 
 ILogger logger = loggerFactory.CreateLogger<Program>();
-
-logger.LogInformation($"THIS IS THE ACTIVE CONFIG: {JsonSerializer.Serialize(builder.Configuration.AsEnumerable())}");
-logger.LogInformation($"THIS IS THE ATTACHED CONFIG: {JsonSerializer.Serialize(configuration.AsEnumerable())}");
 
 builder.Services.ConfigureAutoMapper();
 builder.Services.ConfigureMediatR();
